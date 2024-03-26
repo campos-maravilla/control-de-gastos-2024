353b99d78de9a9d714c7cf9d98b1db80037cb680
campos-maravilla <antonycampos25@outlook.com>
2024-03-26 09:05:34 -0600

    se hace el context,se llama useReducer para llamar a budgetreducer,initialState,luego se hace un type llamado BudgetContextProps y se le pasa el state tipo BdgetState que viene de budget-reducer y tambien se le pasa el dispatch y se le pasa dispatch: Dispatch<BudgetActions> que tambien viene de budget-reducer y se crea un type para children de tipo reactnode

--- src/reducers/budget-reducer.ts
+++ src/reducers/budget-reducer.ts
@@ -10,7 +10,7 @@ export const initialState:BudgetState={
     budget:0
 }
 
-export const budgetReducer=(state:BudgetState=initialState,action:BudgetActions){
+export const budgetReducer=(state:BudgetState=initialState,action:BudgetActions)=>{
     
     if(action.type==='add-budget'){
         return{
