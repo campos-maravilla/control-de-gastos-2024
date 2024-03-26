244586c7985a150686678e3879ce825630b9d261
campos-maravilla <antonycampos25@outlook.com>
2024-03-26 07:27:49 -0600

    se crea el form y el onchange,y se hace la validacio para no escribir ceros  o espacios en blanco y se guarda en la variablr isValid

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
+
 import BudgetForm from "./components/BudgetForm"
 
 function App() {
- 
 
   return (
     <>
-    <header className="bg-blue-400 py-8 max-h-72">
+    <header className="bg-blue-600 py-8 max-h-72">
       <h1 className="uppercase text-center font-black text-4xl text-white">Planificador de Gastos</h1>
     </header>
     
