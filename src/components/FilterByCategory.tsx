b88af510468915915fccb55cd8e10fab80747332
campos-maravilla <antonycampos25@outlook.com>
2024-03-30 10:17:24 -0600

    escribiendo en el state la categoria a filtrar

--- src/components/FilterByCategory.tsx
+++ src/components/FilterByCategory.tsx
@@ -1,8 +1,14 @@
+import { ChangeEvent } from "react";
 import { categories } from "../data/categories";
-
-
+import { useBudget } from "../hooks/useBudget";
 
 export default function FilterByCategory() {
+    const {dispatch}= useBudget() 
+    
+    const handleChange=(e:ChangeEvent<HTMLSelectElement>)=>{
+    dispatch({type:'add-filter-category',payload:{id:e.target.value}})
+    }
+    
   return (
     <div className="mb-10 bg-white shadow-lg rounded-lg p-10">
         <form>
@@ -10,6 +16,7 @@ export default function FilterByCategory() {
                 <label htmlFor="category">Filtrar Gastos</label>
                 <select id="category"
                 className="bg-slate-100 p-3 flex-1 rounded"
+                onChange={handleChange}
                 >
                     <option value="">-- Todas las Categorias --</option>
                     {categories.map(category=>(
--- src/reducers/budget-reducer.ts
+++ src/reducers/budget-reducer.ts
@@ -1,4 +1,4 @@
-import { DraftExpense, Expense } from "../types"
+import { Category, DraftExpense, Expense } from "../types"
 import {v4 as uuidv4} from 'uuid'
 
 export type BudgetActions=
@@ -9,14 +9,16 @@ export type BudgetActions=
 {type:'remove-expense',payload:{id:Expense['id']}}|
 {type:'get-expense-by-id',payload:{id:Expense['id']}}|
 {type:'update-expense',payload:{expense:Expense}}|
-{type:'reseat-app'}
+{type:'reseat-app'}|
+{type:'add-filter-category',payload:{id:Category['id']}}
 
 
 export type BudgetState={
     budget:number
     modal:boolean
     expenses:Expense[]
-    editingId:Expense['id']
+    editingId:Expense['id'],
+    currentCategory:Category['id']
 }
 
 const initialBudget=():number=>{
@@ -35,7 +37,8 @@ export const initialState:BudgetState={
    modal:false,
    //expenses:[],
    expenses:localStorageExpenses(),
-   editingId:''
+   editingId:'',
+   currentCategory:''
 }
 
 const createExpense=(draftExpense:DraftExpense):Expense=>{
@@ -111,6 +114,13 @@ export const budgetReducer=(state:BudgetState=initialState,action:BudgetActions)
             ...state,
       budget:0,
       expenses:[],
+        }
+    }
+    
+    if(action.type==='add-filter-category'){
+        return{
+            ...state,
+            currentCategory:action.payload.id
         }
     }
     
