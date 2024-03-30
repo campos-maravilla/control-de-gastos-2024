3848e3f761d2a4cb4af3525ae2dc8153d1539cf4
campos-maravilla <antonycampos25@outlook.com>
2024-03-29 20:03:02 -0600

    agregando-una-grafica-circular-interactiva,se instala react-circular-progresbar,esto se hace en BudgetTracker

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -1,15 +1,29 @@
 
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 import { useBudget } from "../hooks/useBudget";
 import AmountDisplay from "./AmountDisplay";
+import "react-circular-progressbar/dist/styles.css"
 
 export default function BudgetTracker() {
   
 const {state, totalExpenses,remainBudget}= useBudget()
+
+const percentage=+((totalExpenses/state.budget)*100).toFixed(2)
+//console.log(percentage)
  
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
         <div className="flex justify-center">
-            <img src="/grafico.jpg" alt="Gràfica de gastos" />
+           <CircularProgressbar 
+           value={percentage}
+           styles={buildStyles({
+            pathColor:percentage===100 ? '#DC2626':'#3b82f6',
+            trailColor:'#f0dedbd7',
+            textSize:6,
+            textColor:percentage===100 ? '#DC2626':'#3b82f6',
+           })}
+           text={`${percentage}% Gastado`}
+           />
         </div>
         
         <div className="flex flex-col justify-center items-center gap-8">
