794f9d4c1a1486c66881e0ba4c7d48ff7cb9d71a
campos-maravilla <antonycampos25@outlook.com>
2024-03-30 12:29:38 -0600

    mostrar-los-gastos-de-la-categoria-seleccionada-y-building-de-la-app

--- src/components/ExpenseList.tsx
+++ src/components/ExpenseList.tsx
@@ -6,14 +6,16 @@ import ExpenseDetails from "./ExpenseDetails"
 export default function ExpenseList() {
    const {state}= useBudget()
     
-   const isEmpty=useMemo(()=>state.expenses.length===0,[state.expenses])
    
+   const filterExpenses=state.currentCategory ? state.expenses.filter(expense=>expense.category===state.currentCategory):state.expenses
+   
+   const isEmpty=useMemo(()=>filterExpenses.length===0,[filterExpenses])
   return (
     <div className="bg-white shadow-lg rounded-lg p-10">
         {isEmpty ? <p className="text-gray-600 text-2xl font-bold">No hay Gastos Aún</p>:(
             <>
             <p className="text-gray-600 text-2xl font-bold my-5 ">Listado de Gastos.</p>
-            {state.expenses.map(expense=>(
+            {filterExpenses.map(expense=>(
                 <ExpenseDetails key={expense.id} expense={expense}/>
                 ))}
             </>
